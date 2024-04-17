9e0dc006db75d2ff5df9d37c8a5916d178c8e434
Reddyong <d0209hong@naver.com>
2024-04-17 15:30:09 +0900

    feat : Object Mapper test -> using objectMapper when exchanging json and object

--- src/test/java/com/example/restapi/RestApiApplicationTests.java
+++ src/test/java/com/example/restapi/RestApiApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.restapi;
 
+import com.example.restapi.model.MemberDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestApiApplicationTests {
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
+		var user = new MemberDTO();
+		user.setName("Hong");
+		user.setPhoneNumber("010-1111-1111");
+		user.setEmail("aaaa@naver.com");
+		user.setIsKorean(true);
+
+		var json = objectMapper.writeValueAsString(user);
+		System.out.println("json = " + json);
+
+		var dto = objectMapper.readValue(json, MemberDTO.class);
+		System.out.println("dto = " + dto);
 	}
 
 }
